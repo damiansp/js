f9bad558bcae0f11520ee3297a20d22084832367
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-02-28 15:21:55 -0800

    uses iteration over arrays

--- ninja/quiz/main.js
+++ ninja/quiz/main.js
@@ -1,5 +1,21 @@
-alert('Welcome to Quiz Ninja!');
+//alert('Welcome to Quiz Ninja!');
 
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+//const question = 
+//const answer = prompt(question);
+//alert(`You answered ${answer}`);
+
+const quiz = [
+  ["What is Superman's real name?", 'Clark Kent'],
+  ["What is Wonder Woman's real name?", 'Diana Prince'],
+  ["What is Batman's real name?", 'Bruce Wayne']];
+var score = 0;
+
+for (const [question, answer] of quiz) {
+  const response = prompt(question);
+  if (response == answer) {
+    alert('Correct!');
+    score++;
+  } else { alert(`Wrong! The answer is ${answer}`); }
+}
+
+alert(`Game over. Score: ${score}`);
