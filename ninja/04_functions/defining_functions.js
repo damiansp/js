a6ab83bc2a799ce671333c9d26dc3629f55853db
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-03-06 08:54:14 -0800

    init defining_functions

--- ninja/04_functions/defining_functions.js
+++ ninja/04_functions/defining_functions.js
@@ -1,1 +1,21 @@
-//
+"use strict";
+
+function logArbitraryArgs(...args) {
+  for (arg of args) {
+    console.log(arg);
+  }
+}
+
+logArbitraryArgs(2, 3, 4, 3, 0);
+
+function mean(...values) {
+  let total = 0;
+  for (const val of values) {
+    total += val;
+  }
+
+  return total / values.length;
+}
+
+console.log(mean(1, 2, 3, 5, 7, 11, 13));
+
