e47e28eab5b6469158fdfcc98a3a54af1e4b24f0
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-21 13:00:45 -0700

    +folder/index based imports

---
+++ node/beginningNode/04_nodePackages/folder_based/index_based/bar/bar2.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+  console.log('bar2 called');
+};
---
+++ node/beginningNode/04_nodePackages/folder_based/index_based/foo.js
@@ -0,0 +1,7 @@
+var dotbar = require('./bar');
+dotbar.bar1();
+dotbar.bar2();
+
+var bar = require('bar');
+bar.bar1();
+bar.bar2();
