e81377506a1fea73d64f7f29aa879db9ed04b888
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-09-28 08:17:37 -0700

    methods++

--- rhino/07_Arrays/array_methods.js
+++ rhino/07_Arrays/array_methods.js
@@ -49,4 +49,44 @@ console.log(a.slice(0, 3));   // ['a', 'b', 'c']
 console.log(a.slice(3));      // ['d', 'e']
 console.log(a.slice(1, -1));  // ['b', 'c', 'd']
 console.log(a.slice(-3, -2)); // ['c']
-  
+
+
+// splice (start, n, insert...)
+a = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a.splice(4)); // [5, 6, 7, 8]
+console.log(a);           // [1, 2, 3, 4]
+console.log(a.splice(1, 2)); // [2, 3]
+console.log(a);              // [1, 4]
+
+a = [1, 2, 3, 4, 5];
+console.log(a.splice(2, 0, 'a', 'b')); // []
+console.log(a);                        // [1, 2, 'a', 'b', 3, 4, 5]
+console.log(a.splice(2, 2, [1, 2], 3)); // ['a', 'b']
+console.log(a);                         // [1, 2, [1, 2], 3, 3, 4, 5]
+
+
+// push/pop
+var stack = [];
+console.log(stack.push(1, 2)); // 2
+console.log(stack);            // [1, 2]
+console.log(stack.pop());      // 2
+console.log(stack);            // [1]
+console.log(stack.push([4, 5])); // 2 (length of stack)
+console.log(stack);              // [1, [4, 5]]
+
+
+// shift/unshift
+a = [];
+console.log(a.unshift(1)); // 1 (length of a)
+console.log(a);            // [1]
+console.log(a.unshift(22)); // 2
+console.log(a);             // [22, 1]
+console.log(a.shift());     // 22
+console.log(a);             // [1]
+
+
+// toString()
+console.log([1, 2, 3].toString()); // '1,2,3'
+console.log(['a', 'b', 'c'].toString()); // 'a,b,c'
+console.log([1, ['b', 3]].toString());   // '1,b,3'
+
