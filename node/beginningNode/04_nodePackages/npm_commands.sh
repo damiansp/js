4c54e6405240d6ede783ce0a7c6437ef341dc110
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-04 08:20:40 -0700

    commands++

--- node/beginningNode/04_nodePackages/npm_commands.sh
+++ node/beginningNode/04_nodePackages/npm_commands.sh
@@ -1,4 +1,10 @@
 npm init
 npm install x # installs module x
 npm install x --save # installs and adds dependency to package.json
+npm install x@1.0.3  # spec version
+npm install x@"~1.0.0" # all patch versions of 1.0
+npm install x@"^1.0.0" # all minor version changes
+npm ls                 # list installed packages
+npm rm x               # remove x
+npm unistall x         # same
 
