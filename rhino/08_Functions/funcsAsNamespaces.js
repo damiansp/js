aac3d4f3929ca2095ed8b00c157fb73418cb4132
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-03 20:23:11 -0400

    namespace++

--- rhino/08_Functions/funcsAsNamespaces.js
+++ rhino/08_Functions/funcsAsNamespaces.js
@@ -10,3 +10,31 @@ myModule();
   // myModule code here
 }());
 
+
+var extend = (function() {
+    // Check for known IE bug
+    for (var p in {toString: null}) {
+      // for/in works correctly: return simple extend() function
+      return function extend(o) {
+        for (var i = 1; i < arguments.length; i++) {
+          var source = arguments[i];
+          for (var prop in source) o[prop] = source[prop];
+        }
+        return o;
+      };
+    }
+    // for/in did not enumerate the toString property: return patched version
+    var protoprops = [
+      'toString', 'valueOf', 'constructor', 'hasOwnProperty', 'isPrototypeOf',
+      'propertyIsEnumerable', 'toLocaleString'];
+    return function patched_extend(o) {
+      for (var i = 1; i < arguments.length; i++) {
+        for (var prop in source) o[prop] = source[prop];
+        for (var j = 0; j < protoprops.length; j++) {
+          prop = protoprops[j];
+          if (source.hasOwnProperty(prop)) o[prop] = source[prop];
+        }
+      }
+      return o;
+    };
+}());
