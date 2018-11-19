8cee59f568818cea18636bcab4877a40e7a90d00
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-19 07:43:23 -0800

    +FucnConstr, bind++

---
+++ rhino/08_Functions/FunctionConstructor.js
@@ -0,0 +1,2 @@
+var f = new Function("x", "y", "return x*y;");
+console.log(f(9, 8)); // 72
--- rhino/08_Functions/bind.js
+++ rhino/08_Functions/bind.js
@@ -18,3 +18,16 @@ console.log(succ(2)); // 3
 function f(y, z) { return this.x + y + z }
 var g = f.bind({x: 1}, 2);
 console.log(g(3)); // 6 (this.x: 1 + y: 2 + z: 3)
+
+
+if (!Function.prototype.bind) {
+  Fuction.prototype.bind = function(o /*, args */) {
+    var self = this, boundArgs = argumnets;
+    return function() {
+      var args = [], i;
+      for (i = 1; i < boundArgs.length; i++) args.push(boundArgs[i]);
+      for (i = 0; i < arguments.length; i++) args.push(arguments[i]);
+      return self.apply(o, args);
+    }
+  }
+}
