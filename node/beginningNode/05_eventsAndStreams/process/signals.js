8aca450e8ff2d60a9067632fb920b1d0cf2d5a93
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-26 07:53:46 -0800

    +signals, streams/

---
+++ node/beginningNode/05_eventsAndStreams/process/signals.js
@@ -0,0 +1,8 @@
+setTimeout(
+  function() {
+    console.log('5s passed; exiting.');
+  },
+  5000);
+
+console.log('Started. Will exit in 5s.');
+process.on('SIGINT', function() { console.log('Got SIGINT. Ignoring.'); });
