0e56151fa292a82c0d0056b45ff62bbc07a244bc
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-12-24 10:40:36 -0800

    add conj method to Complex

--- rhino/09_ClassesAndModules/Complex.js
+++ rhino/09_ClassesAndModules/Complex.js
@@ -21,7 +21,7 @@ Complex.prototype.add = function(that) {
 
 
 Complex.prototype.mult = function(that) {
-  retrun new Complex(this.r*that.r - this.i*that.i,
+  return new Complex(this.r*that.r - this.i*that.i,
                      this.r*that.i + this.i*that.r);
 };
 
@@ -34,7 +34,10 @@ Complex.prototype.mag = function() {
 Complex.prototype.neg = function() { return new Complex(-this.r, -this.i); };
 
 
-Complex.protype.toString = function() {
+Complex.prototyp.conj = function() { return new Complex(this.r, -this.i); };
+
+
+Complex.prototype.toString = function() {
   return '{' + this.r + ', ' + this.i + '}';
 };
 
@@ -50,9 +53,9 @@ Complex.prototype.equals = function(that) {
  * Class fields (such as constants) and class methods are defined as props of 
  * the constructor.  Note that class methods DO NOT generally use "this"
  */
-const Complex.ZERO = new Complex(0, 0);
-const Complex.ONE = new Complex(1, 0);
-const Complex.I = new Complex(0, 1);
+Complex.ZERO = new Complex(0, 0);
+Complex.ONE = new Complex(1, 0);
+Complex.I = new Complex(0, 1);
 
 Complex.parse = function(str) {
   try {
@@ -65,3 +68,10 @@ Complex.parse = function(str) {
 
 
 Complex._format = /^\{([^,]+), ([^}]+)\}$/;
+
+
+var c = new Complex(2, 3);      // 2+3i
+var d = new Complex(c.i, c.r);  // 3+2i
+
+console.log(c.add(d).toString()); // {5, 5}
+console.log(Complex.parse(c.toString()).add(c.neg()).equals(Complex.ZERO)); // t
