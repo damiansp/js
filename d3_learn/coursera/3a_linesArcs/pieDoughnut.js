1fa0bbb3ca53bc89d7ec57f2ec366bcf51440188
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-03-14 08:43:01 -0700

    complete 3a (doughnut chart)

--- d3_learn/coursera/3a_linesArcs/pieDoughnut.js
+++ d3_learn/coursera/3a_linesArcs/pieDoughnut.js
@@ -6,12 +6,19 @@ d3.csv("../data/sales.csv").then(showData);
 function showData(data) {
   let bodyHeight = 500;
   let bodyWidth = 500;
+  let pad = 20;
+  let r = bodyHeight / 2;
   data = data.map(d => ({country: d.country, sales: +d.sales}));
   let pie = d3.pie().value(d => d.sales);
-  let colorScale = d3.scaleOridinal()
+  let colorScale = d3.scaleOrdinal()
     .range(d3.schemeCategory10)
     .domain(data.map(d => d.country));
-  
+  let arc = d3.arc().outerRadius(r).innerRadius(r / 2);
+  let doughnutG = body.selectAll('.arc').data(pie(data)).enter().append('g');
+  doughnutG.append('path')
+    .attr('d', arc)
+    .attr('fill', d => { return colorScale(d.data.country) })
+    .attr('transform', 'translate(' +  (r + pad) + ', ' + (r + pad) +  ')');
 }
 
 
