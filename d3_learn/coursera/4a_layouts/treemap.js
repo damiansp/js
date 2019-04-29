9c62cb5590397684cdfd99455bd220e6c37ad92f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-29 07:38:43 -0700

    complete treemap

--- d3_learn/coursera/4a_layouts/treemap.js
+++ d3_learn/coursera/4a_layouts/treemap.js
@@ -6,10 +6,25 @@ d3.json("../data/tree.json").then(showData);
 function showData(data) {
   let bodyHeight = 400;
   let bodyWidth = 400;
-  let treemap = d3.treemap().size([bodyWidth, bodyHeight]);
+  let treemap = d3.treemap().size([bodyWidth, bodyHeight]).paddingInner(1);
   let root = d3.hierarchy(data).sum(d => d.sales);
-  //console.log(root);
-  console.log(treemap(root));
+
+  treemap(root);
+  let cScale = d3.scaleOrdinal(d3.schemeCategory10);
+  let cell = body.selectAll('g')
+    .data(root.leaves())
+    .enter()
+    .append('g')
+    .attr('transform', d => `translate(${d.x0}, ${d.y0})`);
+
+  cell.append('rect')
+    .attr('width', d => d.x1 - d.x0)
+    .attr('height', d => d.y1 - d.y0)
+    .attr('fill', d => cScale(d.parent.data.name));
+  cell.append('text')
+    .text(d => d.data.name)
+    .attr('alignment-baseline', 'hanging')
+    .attr('fill', 'white');
 }
 
 
