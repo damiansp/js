66c57808b62fa5e55a235d1da0fedb6a6fecd6a8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-02-24 08:49:12 -0800

    updated board and square states

--- react/tutorial/tictactoe/src/index.js
+++ react/tutorial/tictactoe/src/index.js
@@ -5,10 +5,15 @@ import './index.css';
   
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: null};
+  }
+  
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
@@ -16,8 +21,13 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {squares: Array(9).fill(null)};
+  }
+  
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
