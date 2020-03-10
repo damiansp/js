91c09d4539075d3e7842ec9b5f93be87740adf11
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-03-10 07:35:54 -0700

    update src/index.js

--- react/tutorial/tictactoe/src/index.js
+++ react/tutorial/tictactoe/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
   
+/* Old 
 
 class Square extends React.Component {
   constructor(props) {
@@ -12,10 +13,19 @@ class Square extends React.Component {
   
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>);
   }
+}
+*/
+
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>);
 }
 
 
@@ -26,7 +36,14 @@ class Board extends React.Component {
   }
   
   renderSquare(i) {
-    return <Square value={i} />;
+    return (<Square value={this.state.squares[i]}
+                   onClick={() => this.handleClick(i)} />);
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); // .slice() gets copy
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
