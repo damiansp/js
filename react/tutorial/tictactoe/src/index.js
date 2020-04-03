175156bc5baaadf340224b7b23eaafa2cad892ce
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-04-03 08:13:33 -0700

    completed tic tac toe game

--- react/tutorial/tictactoe/src/index.js
+++ react/tutorial/tictactoe/src/index.js
@@ -2,23 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-  
-/* Old 
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: null};
-  }
-  
-  render() {
-    return (
-            <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>);
-  }
-}
-*/
 
 
 function Square(props) {
@@ -62,11 +45,13 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {history: [{squares: Array(9).fill(null)}], xIsNext: true};
+    this.state = {history: [{squares: Array(9).fill(null)}],
+                  stepNumber: 0,
+                  xIsNext: true};
   }
   
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice(); // .slice() gets copy
     if (calculateWinner(squares) || squares[i]) {
@@ -74,17 +59,24 @@ class Game extends React.Component {
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({history: history.concat([{squares: squares}]),
+                   stepNumber: history.length,
                    xIsNext: !this.state.xIsNext});
+  }
+
+  jumpTo(step) {
+    this.setState({stepNumber: step, xIsNext: (step % 2) === 0});
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
         const desc = move ? 'Go to move #' + move : 'Go to game start';
         return (
-          <li><button onClick={() => this.jumpTo(move)}>{desc}</button></li>);
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>);
     });
     let status;
 
