df4ecdd36e1a4c7c13fcb9bb23d8e03da53506a5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-03 17:21:47 -0700

    varDeclarationAssign++

--- rhino/03_TypesValsVars/10_variableDeclarationAssignment.js
+++ rhino/03_TypesValsVars/10_variableDeclarationAssignment.js
@@ -1,1 +1,15 @@
-/* ph */
+let o = {x: 1, y: 2};
+
+for (const [name, val] of Object.entries(o)) {
+  console.log(name, val); // x 1  y 2
+}
+
+let [x, ...y] = [1, 2, 3, 4];
+console.log(y); // [2, 3, 4]
+
+let transparent = {r: 0.0, g: 0.0, b: 0.0, a: 1.0};
+let {r, g, b} = transparent;
+console.log(r + " " + g + " " + b); // 0.0 0.0 0.0
+
+  
+  
