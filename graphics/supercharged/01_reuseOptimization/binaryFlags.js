969f9c14d4b43505f416c4388ed6aeaccbb01501
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-06 15:48:19 -0700

    binflags++

--- graphics/supercharged/01_reuseOptimization/binaryFlags.js
+++ graphics/supercharged/01_reuseOptimization/binaryFlags.js
@@ -10,8 +10,28 @@ const CAT = 1,
 let searchFlags = BIG + YOUNG + WHITE;
 let pets = [];
 let nPets = pets.length;
+
 for (let i = 0; i < nPets; i++) {
   if (searchFlags & pets[i].flags === searchFlags) {
     console.log('Found a match!');
   }
 }
+
+
+// cf. much slower:
+let search = ['big', 'young', 'white'];
+let pets = [];
+let nPets = pets.length;
+
+for (let i = 0; i < nPets; i++) {
+  for (let c = 0; c < search.length; c++) {
+    if (pets[i][search[c]] == undefined) { break; }
+  }
+  if (c == search.length) { console.log('Found a match!'); }
+}
+
+toggle ^= 1;             // eq. to:
+toggle = toggle ? 0 : 1; // eq. to
+if (toggle) { toggle = 0; }
+else { toggle = 1; }
+
