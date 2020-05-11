f697e18a35e674e8f643007591bf2a03e673f945
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-11 08:04:00 -0700

    keys++

--- react/tutorial/helloworld/keys.js
+++ react/tutorial/helloworld/keys.js
@@ -28,3 +28,58 @@ function NumberList(props) {
 const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(<NumberList numbers={numbers} />,
                 document.getElementById('root'));
+
+
+/* Keys must only be unique among siblings */
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>{post.title}</li>
+      )};
+    </ul>);
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>);
+  return (
+    <div>
+      {sidebar}
+    <hr />
+      {content}
+    </div>);
+}
+
+
+const posts = [
+  {id: 1, title: 'Hello, World!', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'Installing React from npm.'}];
+
+ReactDOM.render(<Blog posts={posts} />, document.getElementById('root'));
+
+
+/* pass keys explicitly if need to be shared */
+const content = posts.map((post) =>
+  <Post key={post.id} id={post.id} title={post.title} />);
+
+
+/* Embedding map() in JSX */
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <ListItem key={number.toString()} value={number} />);
+  return <ul>{listItems}</ul>;
+}
+
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  return (
+    <ul>
+      {numbers.map((n) =>
+        <ListItem key={n.toString()} value={n} />
+      )}
+    </ul>);
+}
+
