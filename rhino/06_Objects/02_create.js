731f692a3e6dce7ea853d56646bc667cc8bee0fa
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-26 21:07:59 -0700

    +2 create

---
+++ rhino/06_Objects/02_create.js
@@ -0,0 +1,20 @@
+// 1. Object literals
+let empty = {};
+let point = {x: 0, y: 0};
+let p2 = {x: point.x, y: point.y + 1};
+let book = {"title": "JavaScript",
+            "sub-title": "The big one",
+            for: "all",
+            author: { firstname: "Bob", surname: "Dobolina"}};
+
+
+
+// 2. new
+let o = new Object(); // {}
+let a = new Array();  // []
+let d = new Date();
+let m = new Map();
+
+
+
+// 4. Object.create()
