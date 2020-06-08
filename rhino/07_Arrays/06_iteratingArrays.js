bca2abb7fe70fa2d40fef3869263f326503d9726
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-07 19:13:04 -0700

    fin 7.6, +7.7, init 7.8

--- rhino/07_Arrays/06_iteratingArrays.js
+++ rhino/07_Arrays/06_iteratingArrays.js
@@ -8,3 +8,15 @@ for (let [i, letter] of letters.entries()) {
   if (i % 2 === 0) { everyother += letter; }
 }
 console.log(everyother); // hlowrd
+
+
+let uppercase = '';
+letters.forEach(letter => {uppercase += letter.toUpperCase(); });
+console.log(uppercase); // HELLO WORLD
+
+let vowels = '';
+for (let i = 0; i < letters.length; ++i) {
+  let letter = letters[i];
+  if (/[aeiou]/.test(letter)) { vowels += letter; }
+}
+console.log(vowels); // eoo
---
+++ rhino/07_Arrays/07_multidimArrays.js
@@ -0,0 +1,16 @@
+let table = new Array(10);
+for (let i = 0; i < table.length; ++i) {
+  table[i] = new Array(10);
+}
+
+for (let row = 0; row < table.length; ++row) {
+  for (let col = 0; col < table[row].length; ++col) {
+    table[row][col] = row * col;
+  }
+}
+
+function mult(a, b) {
+  return table[a][b];
+}
+
+console.log(mult(7, 8)); // 56
