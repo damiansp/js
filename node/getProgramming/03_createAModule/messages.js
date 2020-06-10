d8818f851f8d7ea168b75d36c622f792b87c0762
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-10 08:10:03 -0700

    init 3 creat module

---
+++ node/getProgramming/03_createAModule/messages.js
@@ -0,0 +1,3 @@
+exports.messages = ["May the force be with you", "Stay on target",
+                    "Be excellent to each other", "Live long and prosper",
+                    "Party on, dude"];               
