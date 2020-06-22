508499f3a7d5723b7d09125862897f4ddda04192
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-21 21:52:16 -0700

    argsParams++

--- rhino/08_Functions/03_argsAndParams.js
+++ rhino/08_Functions/03_argsAndParams.js
@@ -26,3 +26,70 @@ console.log(a); // x y z
 const rectangle = (w, h=2*w) => ({w, h});
 console.log(rectangle(1, 3)); // {w: 1, h: 3}
 console.log(rectangle(1));    // {w: 1, h: 2}
+
+
+// 2. Rest Params and Var-length Args
+function max(first=-Infinity, ...rest) {
+  let maxValue = first;
+  for (let n of rest) {
+    if (n > maxValue) maxValue = n;
+  }
+  return maxValue;
+}
+
+console.log(max(1, 10, 100, 1000, 2, 30, 40)); // 1000
+
+
+// 3. The Arguments Object
+function min(x) {
+  let minValue = Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] < minValue) minValue = arguments[i];
+  }
+  return minValue;
+}
+
+console.log(min(1, 10, 100, 1000, 2, 30, 40)); // 1
+
+
+// 4. Spread Operator
+let numbers = [5, 2, 10, -1, 9, 100, 4];
+console.log(Math.min(...numbers));
+
+
+function timed(f) {
+  return function(...args) {
+    console.log(`Entering function ${f.name}`);
+    let startTime = Date.now();
+    try { return f(...args); }
+    finally {
+      console.log(`Exiting ${f.name} after ${Date.now() - startTime} ms`);
+    }
+  };
+}
+
+
+function benchmark(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) sum +=i;
+  return sum;
+}
+
+timed(benchmark)(1000000);
+
+
+// 5. Destructuring Function Args into Params
+function vectorAdd(v1, v2) {
+  return [v1[0] + v2[0], v1[1] + v2[1]]
+}
+
+console.log(vectorAdd([1, 2], [3, 4])); // [4, 6]
+
+
+function vectorAdd2([x1, y1], [x2, y2]) {
+  return [x1 + x2, y1 + y2]
+}
+
+console.log(vectorAdd2([1, 2], [3, 4])); // [4, 6]
+
+
