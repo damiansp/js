3bce2e2d6f01adcc787bf77d2c2e40cb31bf19f5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-26 07:32:26 -0700

    init main.js, index.html

--- node/getProgramming/06_betterRoutesAndFileServing/serveHTML/main.js
+++ node/getProgramming/06_betterRoutesAndFileServing/serveHTML/main.js
@@ -1,1 +1,26 @@
-// ph
+const http = require('http'),
+  httpStatus = require('http-status-codes'),
+  fs = require('fs');
+
+const port = 3000,
+  routeMap = {'/': 'views/index.html'};
+
+
+const getViewURL = (url) => { return `views${url}.html`; };
+
+
+http.createServer((req, res) => {
+    let viewURL = getViewURL(req.url);
+    fs.readFile(viewURL, (error, data) => {
+        if (error) {
+          res.writeHead(httpStatus.NOT_FOUND);
+          res.write('<h1>FILE NOT FOUND</h1>');
+        } else {
+          res.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
+          res.write(data);
+        }
+        res.end();
+    });
+}).listen(port);
+
+console.log(`Server running and listening on port ${port}`);
