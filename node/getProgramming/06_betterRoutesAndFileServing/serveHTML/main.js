e5654693b054bc38aec839c0b789c30b1d003eab
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-06-29 21:45:30 -0700

    new main.js, old -> 1

--- node/getProgramming/06_betterRoutesAndFileServing/serveHTML/main.js
+++ node/getProgramming/06_betterRoutesAndFileServing/serveHTML/main.js
@@ -5,22 +5,45 @@ const http = require('http'),
 const port = 3000,
   routeMap = {'/': 'views/index.html'};
 
-
 const getViewURL = (url) => { return `views${url}.html`; };
+
+const sendErrorResponse = res => {
+  res.writeHead(httpStatus.NOT_FOUND, {'Content-Type': 'text/html'});
+  res.write('<h1>File Not Found!</h1>');
+  res.end();
+};
 
 
 http.createServer((req, res) => {
-    let viewURL = getViewURL(req.url);
-    fs.readFile(viewURL, (error, data) => {
-        if (error) {
-          res.writeHead(httpStatus.NOT_FOUND);
-          res.write('<h1>FILE NOT FOUND</h1>');
-        } else {
-          res.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
-          res.write(data);
-        }
-        res.end();
-    });
+    let url = req.url;
+    if (url.indexOf('.html') !== -1) {
+      res.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
+      customReadFile(`./public/viewss${url}`, res);
+    } else if (url.indexOf('.js') !=== -1) {
+      res.writeHead(httpStatus.OK, {'Content-Type': 'text/javascript'});
+      customReadFile(`./public/js${url}`, res);
+    } else if (url.indexOf('.css') !== -1) {
+      res.writeHead(httpStatus.OK, {'Content-Type': 'text/css'});
+      customReadFile(`./public/css${url}`, res);
+    } ese if (url.indexOf('.png') !== -1) {
+      res.writeHead(httpStatus.OK, {'Content-Type': 'image/png'});
+      customReadFile(`./public/images${url}`, res);
+    } else sendErrorResponse(res);
 }).listen(port);
 
-console.log(`Server running and listening on port ${port}`);
+console.log(`The server is listening on port ${port}`);
+
+
+const customReadFile = (filePath, res) => {
+  if (fs.existsSync(filePath)) {
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+          console.log(error);
+          sendErrorResponse(res);
+          return;
+        }
+        res.write(data);
+        res.end();
+    });
+  } else sendErrorResponse(res);
+};
