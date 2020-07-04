7b55d86b4f192946fe219d370dd3b1c494e99ead
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-03 19:54:05 -0700

    delete ninja

--- ninja/02_basics/symbols.js
+++
@@ -1,8 +0,0 @@
-const uniqueID = Symbol('this is a unique ID');
-console.log(uniqueID);
-console.log(typeof uniqueID);
-console.log(String(uniqueID));
-
-const B = Symbol.for('this is a unique ID');
-console.log(uniqueID == B);
-console.log(uniqueID === B);
--- ninja/02_basics/template_literals.js
+++
@@ -1,5 +0,0 @@
-console.log(`Hello!`);
-console.log(`She said, "It's me!"`);
-
-const name = `Jimbo`;
-console.log(`Hello, ${name}!`);
