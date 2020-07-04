c2ae723e507f35ac2ff29f454c1675dc8ca1a4f5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-04 07:34:44 -0700

    fin 8

--- rhino/08_Functions/08_functionalProgramming.js
+++ rhino/08_Functions/08_functionalProgramming.js
@@ -89,3 +89,34 @@ console.log(partial(f, undefined, 2)(3, 4)); // 3 * (2 - 4) = -6
 const decr = partialLeft(sum, -1);
 const cuberoot = partialRight(Math.pos, 1/3);
 console.log(cuberoot(decr(28))); // 3
+
+
+
+// 4. Memoization
+function memoize(f) {
+  const cache = new Map();
+  return function(...args) {
+    let key = args.length + args.join('+');
+    if (cache.has(key)) return cache.get(key);
+    else {
+      let result = f.apply(this, args);
+      cache.set(key, result);
+      return result;
+    }
+  }
+}
+
+
+function gcd(a, b) {
+  if (a < b) [a, b] = [b, a];
+  while (b !== 0) [a, b] = [b, a % b];
+  return a;
+}
+
+const gcdMemo = memoize(gcd);
+console.log(gcdMemo(85, 187)); // 17
+
+
+const factorial = memoize(function(n) {
+    return (n <= 1) ? 1 : n * factorial(n - 1);
+});
