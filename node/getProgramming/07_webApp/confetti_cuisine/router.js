5db42bd38aaa828b12a89bb295d7a379175edc84
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-05 22:25:43 -0700

    +router, contact.html form

---
+++ node/getProgramming/07_webApp/confetti_cuisine/router.js
@@ -0,0 +1,20 @@
+const httpStatus = require('https-status-codes');
+const  contentTypes = require('./contentTypes'),
+  utils = require('./utils');
+
+const routes = {'GET': {}, 'POST': {}};
+
+
+exports.handle = (req, res) => {
+  try { routes[req.method][req.url](req, res); }
+  catch (e) {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile('views/error.html', res);
+  }
+};
+
+
+exports.get = (url, action) => { routes['GET'][url] = action; };
+
+
+exports.post = (url, action) => { routes['POST'][url] = action; };
