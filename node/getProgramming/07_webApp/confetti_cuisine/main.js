b30986e8f577304aa21e99743a63717cf60f327f
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-05 17:29:32 -0700

    init confetti_cuisine's various

---
+++ node/getProgramming/07_webApp/confetti_cuisine/main.js
@@ -0,0 +1,9 @@
+const http = require('http'),
+  httpStatus = require('http-status-codes');
+
+const router = require('./router'),
+  contentTypes = require('./contentTypes'),
+  utils = require('./utils');
+
+const port = 3000;
+
