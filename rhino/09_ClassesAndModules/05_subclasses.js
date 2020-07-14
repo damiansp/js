39adc4829ccf2bd11703cc6127ffd58e98f23228
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-13 19:10:29 -0700

    5subclasses++

--- rhino/09_ClassesAndModules/05_subclasses.js
+++ rhino/09_ClassesAndModules/05_subclasses.js
@@ -1,1 +1,64 @@
+class Range {
+  constructor(from, to) {
+        // Store state of new object; noninherited properties unique to this obj
+    this.from = from;
+    this.to = to;
+  }
+
+  includes(x) { return this.from <= x && x <= this.to; }
+
+    // Generator func that makes instances iterable (numeric ranges only)
+  *[Symbol.iterator]() {
+    for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+  }
+
+  toString() { return `(${this.from}...${this.to})`; }
+
+  static parse(s) {
+    let matches = s.match(/^\((\d+)\.\.\.(\d+)\)$/);
+    if (!matches) throw new TypeError(`Cannot parse Range from "${s}".`);
+    return new Range2(parseInt(matches[1]), parseInt(matches[2]));
+  }
+}
+
+
 // 1. Subclasses and Prototypes
+function Span(start, span) {
+  if (span >= 0) {
+    this.from = start;
+    this.to = start + span;
+  } else {
+    this.to = start;
+    this.from = start + span;
+  }
+}
+
+
+Span.prototype = Object.create(Range.prototype);
+// Don't want to inherit Range.prototype.constrcutor so
+Span.prototype.constructor = Span;
+// Override
+Span.prototype.toString = function() {
+  return `(${this.from}... +${this.to - this.from})`;
+};
+
+
+
+// 2. Subclasses with extend and super
+class EZArray extends Array {
+  get first() { return this[0]; }
+  get last() { return this[this.length - 1]; }
+}
+
+let a = new EZArray();
+console.log(a instanceof EZArray); // true
+console.log(a instanceof Array);   // true
+a.push(1, 2, 3, 4);
+a.pop();
+console.log(a.first);              // 1
+console.log(a.last);               // 3
+console.log(a[1]);                 // 2
+console.log(Array.isArray(a));     // true
+console.log(EZArray.isArray(a));   // true
+console.log(Array.prototype.isPrototypeOf(EZArray.prototype)); // true
+console.log(Array.isPrototypeOf(EZArray));                     // true
