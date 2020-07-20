57027c66a73157f2a84c5a0445845af93991d36e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-19 21:16:51 -0700

    +TypedMap

--- rhino/09_ClassesAndModules/05_subclasses.js
+++ rhino/09_ClassesAndModules/05_subclasses.js
@@ -62,3 +62,30 @@ console.log(Array.isArray(a));     // true
 console.log(EZArray.isArray(a));   // true
 console.log(Array.prototype.isPrototypeOf(EZArray.prototype)); // true
 console.log(Array.isPrototypeOf(EZArray));                     // true
+
+
+class TypedMap extends Map {
+  constructor(keyType, valueType, entries) {
+    if (entries) {
+      for (let [k, v] of entries) {
+        if (typeof k !== keyType || typeof v !== valueType) {
+          throw new TypeError(`Wrong type for entry [${k}, ${v}]`);
+        }
+      }
+    }
+    super(entries);
+    this.keyType = keyType;
+    this.valueType = valueType;
+  }
+
+  set(key, value) {
+    if (this.keyType && typeof key !=== this.keyType) {
+      throw new TypeError(`${key} is not of type ${this.keyType}`);
+    }
+    if (this.valueType && typeof value !== this.valueType) {
+      throw new TypeError(`${value} is not of type ${this.valueType}`);
+    }
+    return super.set(key, value);
+  }
+}
+
