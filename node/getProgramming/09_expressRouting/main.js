a2d8161ed45c3fd7dd45bf1e3fbde246d593ab5d
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-19 21:05:45 -0700

    main + app.use

--- node/getProgramming/09_expressRouting/main.js
+++ node/getProgramming/09_expressRouting/main.js
@@ -8,4 +8,9 @@ app.get('/items/:vegetable', (req, res) => {
     res.send(`This is the page for ${veg}`);
 });
 
+app.use((req, res, next) => {
+    console.log(`request made to ${req.url}`);
+    next();
+});
+
 app.listen(port, () => { console.log(`Server running on port ${port}`); });
