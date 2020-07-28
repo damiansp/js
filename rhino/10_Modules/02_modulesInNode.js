c4601004e6f31119a8e0913e582423c80b17a1fe
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-27 21:44:39 -0700

    modulesInNode++

--- rhino/10_Modules/02_modulesInNode.js
+++ rhino/10_Modules/02_modulesInNode.js
@@ -1,1 +1,35 @@
 // 1. Node Exports
+const sum = (x, y) => x + y;
+const square = x => x * x;
+
+exports.mean = data => data.reduce(sum) / data.length;
+exports.sd = function(d) {
+  let m = exports.mean(d);
+  return Math.sqrt(d.map(x => x - m).map(square).reduce(sum) / (d.length - 1));
+};
+
+/*
+module.exports = class BitSet extends AbstractWritableSet { ... };
+*/
+
+const sum2 = (x, y) => x + y;
+const square2 = x => x * x;
+
+exports.mean2 = data => data.reduce(sum) / data.length;
+exports.sd2 = d => {
+  let m = exports.mean(d);
+  return Math.sqrt(d.map(x => x - m).map(square).reduce(sum) / (d.length - 1));
+};
+
+module.exports = { mean2, sd2 };
+
+
+
+// 2. Node Imports
+const fs = require('fs');
+const http = require('http');
+/*
+// import own code
+const stats = require('./stats.js')
+const BitSet = require('./utils/bitset.js')
+ */
