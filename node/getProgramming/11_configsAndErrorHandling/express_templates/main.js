c20eb13acef95688b6b67245f2652bb329b4d3de
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-31 07:38:35 -0700

    init configs & err handling

---
+++ node/getProgramming/11_configsAndErrorHandling/express_templates/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+
+const errorController = require('./controllers/errorController');
+const homeController = require('./controllers/homeController');
+
+
+const port = 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || port);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(layouts);
+app.use(errorController.logErrors);
+app.use((req, res, next) => {
+    console.log(`request made to: ${req.url}`);
+    next();
+});
+
+app.get('/name', homeController.respondWithName);
+app.get('/name/:myName', homeController.respondWithName);
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send('POST Successful!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at "http://localhost:${app.get('port')}"`);
+});
+
+
+/** run with, e.g., localhost:3000/name/:Bob */
