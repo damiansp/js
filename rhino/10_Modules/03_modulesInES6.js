d029e0ae91190474679cee453cbb39ae19e91999
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-08-01 18:21:27 -0700

    mods ES6++

--- rhino/10_Modules/03_modulesInES6.js
+++ rhino/10_Modules/03_modulesInES6.js
@@ -7,3 +7,18 @@ export class Circle {
   constructor(r) { this.r = r }
   area() { return PI * this.r * this.r; }
 };
+
+// In place of `exports` throughout, can collect all at the end of the file
+export { Circle, degreesToRadians, PI };
+
+
+
+// 2. ES6 Imports
+import BitSet from './bitset.js';
+import { mean, sd } fro './stats.js';
+import * as stats from './stats.js';
+import './analytics.js'; // case when module has no exports
+
+
+
+// 3. Imports and Exports with Renaming
