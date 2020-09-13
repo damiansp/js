46ba319318639e6c4a3dc6cdb479dc51407ed4a5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-13 16:59:12 -0700

    init todolist

---
+++ react/learning/15_toDoList/todolist/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+
+let dest = document.querySelector('#container');
+
+ReactDOM.render(<div><p>Hello!</p></div>, dest);
