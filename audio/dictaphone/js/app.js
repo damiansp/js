6353230a46f5d3bebc4ea8034772e98c63fd9dee
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-17 13:08:52 -0700

    app.js++

--- audio/dictaphone/js/app.js
+++ audio/dictaphone/js/app.js
@@ -1,68 +1,79 @@
 const record = document.querySelector('.record');
 const stop = document.querySelector('.stop');
 const soundClips = document.querySelector('.soundClips');
-const mediaRecorder = new MediaRecorder(stream);
+const canvas = document.querySelector('.visualizer');
+const ctx = canvas.getContext('2d');
+const mainSection = document.querySelector('.main-controls');
+let audioCtx;
 
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+stop.disabled = true; // disable while not recording
+
+
+if (navigator.mediaDevices.getUserMedia) {
   console.log('getUserMedia supported');
-  navigator.mediaDevices.getUserMedia({audio: true})
-    .then(function(stream) {
+  const constraints = {audio: true};
+  let chunks = [];
 
-    }).catch(function(err) {
-        console.log('The following getUserMedia error occured: ' + err);
-    });
-                                  
-} else console.log('getUserMedia not supported on your browser');
+  let onSuccess = function(stream) {
+    const mediaRecorder = new MediaRecorder(stream);
 
+    visualize(stream);
 
-record.onclick = function() {
-  mediaRecorder.start();
-  console.log(mediaRecorder.state);
-  console.log('recorder started');
-  record.style.background = 'red';
-  record.style.color = 'black';
-};
+    record.onclick = function() {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+      console.log('Recorder started');
+      record.style.background = 'red';
+      stop.disabled = false;
+      record.disabled = true;
+    };
 
+    stop.onclick = function() {
+      mediaRecorder.stop();
+      console.log(mediaRecorder.state);
+      console.log('Recorder stopped')
+      record.style.background = '';
+      record.style.color = '';
+      stop.disabled = true;
+      record.disabled = false;
+    };
 
-let chunks = [];
+    mediaRecorder.onstop = function(e) {
+      console.log('Data available after MediaRecorder.stop() called');
+      const clipName = prompt('Enter a name for your clip:', 'My clip');
+      const clipLabel = document.createElement('p');
+      const audio = document.createElement('audio');
+      const deleteButton = document.createElement('button');
 
-mediaRecorder.ondataavailable = function(e) { chunks.push(e.data); };
+      clipContainer.classList.add('clip');
+      audio.setAttribute('controls', '');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'delete';
+      if (clipName === null) clipLabel.textContent = 'My Clip';
+      else clipLabel.textContext = clipName; 
+      clipContainer.appendChild(audio);
+      clipContainer.appendChild(clipLabel);
+      clipContainer.appendChild(deleteButon);
+      soundClips.appendChild(clipContainer);
+      audio.controls = true;
+      const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+      const audioURL = window.URL.createObjectURL(blob);
 
+      chunks = []; /* *** */
+      audio.src = audioURL;
+      console.log('Recorder stopped');
 
-stop.onclick = function() {
-  mediaRecorder.stop();
-  console.log(mediaRecorder.state);
-  console.log('recorder stopped');
-  record.style.background = '';
-  record.style.color = '';
-};
+      deleteButton.onclick = function() {
+        let eTarget = e.target;
 
-// Grabbing and using the blob
-mediaRecorder.onstop = function(e) {
-  console.log('recorder stopped');
-  const clipName = prompt('Enter a name for your sound clip');
-  const clipContainer = document.createElement('p');
-  const audio = document.createElement('audio');
-  const deleteButton = document.createElement('button');
+        eTarget.parentNode.parentNode.removeChild(eTarget.parentNode);
+      };
 
-  clipContainer.classList.add('clip');
-  audio.etAttribute('controls', '');
-  deleteButton.innerHTML = 'Delete';
-  clipLabel.innerHTML = clipName;
-  clipContainer.appendChild(audio);
-  clipConatiner.appendChild(clipLabel);
-  clipContainer.appendChild(deleteButton);
-  clipContainer.appendChild(clipContainer);
-  const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
-  const audioURL = window.URL.createObjectURL(blob);
+      clipLabel.onclick = function() {
+        ; 
+      };
+    };
 
-  audio.src = audioURL;
-  chunks = [];
-
-  deleteButton.onclick = function(e) {
-    let evtTgt = e.target;
-    evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-  }
-};
-
+  }; // onSuccess
+}
