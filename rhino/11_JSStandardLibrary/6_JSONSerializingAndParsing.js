503e3d9b821be95280e51a62199d86e913bec844
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-18 07:15:36 -0700

    fin 6/json, init 7/intlztn

--- rhino/11_JSStandardLibrary/6_JSONSerializingAndParsing.js
+++ rhino/11_JSStandardLibrary/6_JSONSerializingAndParsing.js
@@ -7,3 +7,30 @@ console.log(copy); // { s: '', n: 0, a: [ true, false, null ] }
 
 // works, but not esp effficient
 function deepcopy(o) { return JSON.parse(JSON.stringify(o)); }
+
+
+console.log(JSON.stringify(o, null, 2)); // adds formatting
+
+
+
+// 1. JSON Customizations
+let data = JSON.parse(text, function(key, value) {
+    // remove props starting with _
+    if (key[0] === '_') return undefined;
+
+    // Convert ISO dates
+    if (typeof value === 'string'
+        && /^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ$/.test(value)) {
+      return new Date(value);
+    }
+
+    // All others unchanged
+    return value;
+});
+
+
+// what fields to stingify, and in what order?
+let text = JSON.stringify(address, ['city', 'state', 'country']);
+
+// skip regex props
+let json = JSON.stringify(o, (k, v) => instanceof RegExp ? undefined : v);
