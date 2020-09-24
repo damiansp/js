61535fa76acf8367915403d141d465a01d6cf8af
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-09-24 14:03:47 -0700

    intl++

--- rhino/11_JSStandardLibrary/7_internationalizationAPI.js
+++ rhino/11_JSStandardLibrary/7_internationalizationAPI.js
@@ -1,1 +1,17 @@
 // 1. Formatting Numbers
+let euros = Intl.NumberFormat('fr', {style: 'currency', currency: 'EUR'});
+console.log(euros.format(10000));
+
+let pounds = Intl.NumberFormat('en', {style: 'currency', currency: 'GBP'});
+console.log(pounds.format(10000));
+
+let data = [0.05, .76, 1];
+let formatData = Intl.NumberFormat(undefined,
+                                   {style: 'percent',
+                                    minimumFractionDigits: 1,
+                                    maximumFractionDigits: 2})
+  .format;
+console.log(data.map(formatData));
+
+
+
