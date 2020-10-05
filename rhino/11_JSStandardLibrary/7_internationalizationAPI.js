a5f432951803c6d31e85919c5805f705780e32d1
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-05 06:33:27 -0700

    intntlztn++

--- rhino/11_JSStandardLibrary/7_internationalizationAPI.js
+++ rhino/11_JSStandardLibrary/7_internationalizationAPI.js
@@ -37,3 +37,36 @@ console.log(Intl.DateTimeFormat('es-ES', opts).format(d));
 // The time in NY fo a French-speaking Canadian:
 opts = {hour: 'numeric', minute: '2-digit', timeZone: 'America/New_York'};
 console.log(Intl.DateTimeFormat('fr-CA', opts).format(d)); // 8 h 14
+
+opts = {year: 'numeric', era: 'short'};
+Intl.DateTimeFormat('en', opts).format(d);
+for (let culture of ['iso8601', 'hebrew', 'buddhist', 'islamic', 'persian',
+                     'indian', 'chinese', 'japanese']) {
+  console.log(
+    culture + ': ' + Intl.DateTimeFormat(`en-u-ca-${culture}`, opts).format(d));
+}
+
+console.log(Intl.DateTimeFormat('en', opts).format(d));
+
+
+
+// 3. Comparing Strings
+const collator = new Intl.Collator().compare;
+console.log(['a', 'z', 'A', 'Z'].sort(collator)); // a A z Z
+
+const filenameOrder = new Intl.Collator(undefined, {numeric: true}).compare;
+console.log(['page10', 'page9', 'page80'].sort(filenameOrder)); // p9, 10, 80
+
+const fuzzyMatcher = new Intl.Collator(
+  undefined, {sensitivity: 'base', ignorePunctuation: true}
+).compare;
+let strings = ['food', 'fool', 'Føø Bar'];
+console.log(strings.findIndex(s => fuzzyMatcher(s, 'foobar') === 0)); // 2
+
+// Pre-1994 'CH', 'LL' and 'RR' were treated as single glyphs
+const modernSpanish = Intl.Collator('es-ES').compare;
+const traditionalSpanish = Intl.Collator('es-ES-u-co-trad').compare;
+let palabras = ['luz', 'llama', 'como', 'chico', 'peru', 'perro'];
+console.log(palabras.sort(modernSpanish));
+console.log(palabras.sort(traditionalSpanish)); // doesn't work
+  
