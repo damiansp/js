18d8ae408b24dc6024a7b4b8265f23aee5325551
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-10-19 08:10:29 -0700

    slider plugin refactor and ++

--- jquery/beginning/11_imageSlider/js/slider.jquery.js
+++ jquery/beginning/11_imageSlider/js/slider.jquery.js
@@ -10,9 +10,41 @@
         let $buttons = {forward: $allButtons.filter('.forward'),
                         back: $allButtons.filter('.back')};
 
+        $allButtons.on('click', function(e) {
+            let isBackButton = $(this).hasClass('back');
+            if (!isBackButton && isAtEnd()) {
+              animateSliderToMargin(0, 1000);
+            } else if (isBackButton && isAtBeginning) {
+              animateSliderToMargin(endMargin, 1000);
+            } else {
+              animateSlider((isBackButton ? '+' : '-'), 1000);
+            }
+            e.preventDefault();
+        });
+
+        let getLeftMargin = function() {
+          return parseInt($sliderList.css('margin-left'), 10);
+        };
+        
+        let isAtBeginning = function() {
+          return getLeftMargin() === 0;
+        };
+
+        let endMargin = -(($sliderItems.length - 1)
+                          * $sliderItems.first().children('img').width());
+        
+        let isAtEnd = function() {
+          return getLeftMargin() === endMargin;
+        };
+
         let animateSlider = function(direction, duration, callback) {
           $sliderList.stop(true, true)
             .animate({'margin-left': direction + '=300px'}, duration, callback);
+        };
+
+        let animateSliderToMargin = function(margin, duration, callback) {
+          $sliderList.stop(true, true)
+            .animate({'margin-left': margin}, duration, callback); 
         };
     });
   };
