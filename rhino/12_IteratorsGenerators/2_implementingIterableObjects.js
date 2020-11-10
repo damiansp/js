fa3e5e6fc50fe0626aeb04cb93ae353bf9460185
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-10 10:52:05 -0800

    iter++

--- rhino/12_IteratorsGenerators/2_implementingIterableObjects.js
+++ rhino/12_IteratorsGenerators/2_implementingIterableObjects.js
@@ -17,7 +17,7 @@ class Range {
   // Make a Range iterable by returning an iterator object. Note that the name
   // of this method is a special symbol, not a string.
   [Symbol.iterator]() {
-    // Each iterator instance must iterate the range independently of others. So
+    // Each iterator instance must iterate the range independnently of others. So
     // we need a state var to track our location in the iteration.  We start at
     // the first int >= from
     let next = Math.ceil(this.from);
@@ -37,3 +37,58 @@ class Range {
 
 for (let x of new Range(1, 10)) { console.log(x); }
 console.log([...new Range(-2, 3)]);
+
+
+// Return an iterable obj that iterates the result of applying f() to each val
+// of the source iterable
+function map(iterable, f) {
+  let iterator = iterable[Symbol.iterator]();
+  return {
+    [Symbol.iterator]() { return this; },
+    next() {
+      let v = iterator.next();
+      if (v.done) return v;
+      return {value: f(v.value)};
+    }
+  };
+}
+
+console.log([...map(new Range(1, 4), x => x*x)]); // 1 4 9 16
+
+
+// Return an iterable obj that filters the specified iterable, iterating only
+// those elements for which the predicate is true
+function filter(iterable, predicate) {
+  let iterator = iterable[Symbol.iterator]();
+  return {
+    [Symbol.iterator]() { return this; },
+    next() {
+      for (;;) {
+        let v = iterator.next();
+        if (v.done || predicate(v.value)) return v;
+      }
+    }
+  };
+}
+
+console.log([...filter(new Range(1, 10), x => x % 3 === 0)]); // 3 6 9
+
+
+function words(s) {
+  var r = /\s+|$/g; // one or more spaces or end
+  r.lastIndex = s.match(/[^ ]/).index; // start at first non-space
+  return {
+    [Symbol.iterator]() { return this; },
+    next() {
+      let start = r.lastIndex;
+      if (start < s.length) {
+        let match = r.exec(s);
+        if (match) { return {value: s.substring(start, match.index)}; }
+      }
+      return {done: true};
+    }
+  };
+}
+
+console.log([...words('  abc def ghi! j.k  ')]); // [abc def ghi! j.k]
+
