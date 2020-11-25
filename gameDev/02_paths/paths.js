16176a12fc90a2ead51fba01b396330eff922dbd
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-25 07:30:30 -0800

    paths++

--- gameDev/02_paths/paths.js
+++ gameDev/02_paths/paths.js
@@ -2,27 +2,39 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 const H = canvas.height;
 const W = canvas.width;
-const GRID_SPACE = 10;
-const MAIN_SPACE = 50;
+const MINOR_SPACE = 10;
 
 
-function drawGrid() {
-  ctx.strokeStyle = '#0F0';
-  for (let y = 0; y < H; y += GRID_SPACE) {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(W, y);
-    ctx.lineWidth = (y % MAIN_SPACE == 0) ? 0.5 : 0.25;
-    ctx.stroke();
-  }
-  for (let x = 0; x < W; x += GRID_SPACE) {
+function drawGrid(ctx, minor, major, stroke, fill) {
+  minor = minor || MINOR_SPACE;
+  major  = major || 5 * minor;
+  stroke = stroke || '#0F0';
+  fill = fill || '#090';
+  ctx.save();
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  for (let x = 0; x < W; x += minor) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, H);
-    ctx.lineWidth = (x % MAIN_SPACE == 0) ? 0.5 : 0.25;
+    ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
     ctx.stroke();
+    if (x % major == 0) ctx.fillText(x, x, 10);
   }
+  for (let y = 0; y < H; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(W, y);
+    ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+    ctx.stroke();
+    if (y % major == 0) ctx.fillText(y, 0, y + 10);
+  }
+  ctx.restore();
 }
 
 
-drawGrid();
+//drawGrid(ctx);
+drawGrid(ctx, 10, 50, 'yellow', 'red');
+//drawGrid(ctx, 5, 30, 'white', 'red');
+
+
