87fc4f2692d1df240df27029f4a9dfeba87fa192
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-11-30 07:22:49 -0800

    fin paths

--- gameDev/02_paths/paths.js
+++ gameDev/02_paths/paths.js
@@ -4,26 +4,30 @@ const H = canvas.height;
 const W = canvas.width;
 
 
+function drawGridLines(ctx, minor, major, xy) {
+  let HW = xy == 'x' ? H : W;
+  for (let p = 0; p < HW; p += minor) {
+    ctx.beginPath();
+    let start = xy == 'x' ? [p, 0] : [0, p];
+    let end = xy == 'x' ? [p, HW] : [HW, p];
+    ctx.moveTo(...start);
+    ctx.lineTo(...end);
+    ctx.lineWidth = (p % major == 0) ? 0.5 : 0.25;
+    ctx.stroke();
+    if (p % major == 0) {
+      let textLoc = xy == 'x' ? [p, p, 10] : [p, 0, p + 10];
+      ctx.fillText(...textLoc);
+    }
+  }
+}
+
+
 function drawGrid(ctx, minor=10, major=50, stroke='#0F0', fill='#090') {
   ctx.save();
   ctx.strokeStyle = stroke;
   ctx.fillStyle = fill;
-  for (let x = 0; x < W; x += minor) {
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, H);
-    ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
-    ctx.stroke();
-    if (x % major == 0) ctx.fillText(x, x, 10);
-  }
-  for (let y = 0; y < H; y += minor) {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(W, y);
-    ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
-    ctx.stroke();
-    if (y % major == 0) ctx.fillText(y, 0, y + 10);
-  }
+  drawGridLines(ctx, minor, major, 'x');
+  drawGridLines(ctx, minor, major, 'y');
   ctx.restore();
 }
 
@@ -53,6 +57,29 @@ function drawLines(ctx, zag, close, stroke='#FFF', fill='#0F0') {
 }
 
 
+function drawCurve(ctx, stroke, fill) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(50, 250);
+  ctx.quadraticCurveTo(25, 300, 50, 350);
+  ctx.quadraticCurveTo(100, 375, 150, 350);
+  ctx.closePath();
+  ctx.moveTo(230, 360);
+  ctx.quadraticCurveTo(255, 340, 270, 360);
+  ctx.quadraticCurveTo(255, 340, 270, 310);
+  ctx.closePath();
+  ctx.moveTo(250, 50);
+  ctx.quadraticCurveTo(310, 60, 370, 50);
+  ctx.quadraticCurveTo(400, 75, 370, 100);
+  ctx.closePath();
+  ctx.strokeStyle = stroke;
+  ctx.fillStyle = fill;
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
+}
+
+
 drawGrid(ctx, 10, 50, 'yellow', 'red');
 drawLines(ctx, zag=true, close=true);//, stroke='#FED', fill=false);
-
+drawCurve(ctx, 'white', 'purple');
