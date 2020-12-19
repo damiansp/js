23b1ce10e9325c65eef857dfad5f3bd4818a04ed
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-12-18 21:25:30 -0800

    async callback++

--- rhino/13_asynchronous/01_asyc_with_callbacks.js
+++ rhino/13_asynchronous/01_asyc_with_callbacks.js
@@ -27,3 +27,30 @@ okay.addEventListener('click', applyUpdate);
 
 
 // 3. Network Events
+function getCurrentVersionNumber(versionCallback) {
+  // Make a scripted http request to a backend version API
+  let request = new XMLHttpRequest();
+  request.open('GET', 'http://www.example.com/api/version');
+  request.send();
+
+  // Register a callback that will be invoked when the response arrives
+  request.onload = function() {
+    if (request.status === 200) {
+      // If http status is good, get version no. and call callback
+      let currentVersion = parseFloat(request.responseText);
+      versionCallback(null, currentVersion);
+    } else {
+      // Otherwise report an error to the callback
+      versionCallback(responsse.statusText, null);
+    }
+  };
+
+  // Register another callback that will be invoked for network errors
+  request.onerror = request.ontimeout = function(e) {
+    versionCallback(e.type, null);
+  };
+}
+
+
+
+// 4. Callbacks and Events in Node
