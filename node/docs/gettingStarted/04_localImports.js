b329b239321ce9880d0e4f53f898163fa5bc04b9
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-01-12 09:54:46 -0800

    +car, 4/local imports

---
+++ node/docs/gettingStarted/04_localImports.js
@@ -0,0 +1,10 @@
+//const library = require('./library') // from my local library.js file
+const car = require('./car');
+
+// or
+// const items = require('./car');
+// const car = itmes.car;
+// or
+// const car = require('./car').car
+
+console.log(car);
