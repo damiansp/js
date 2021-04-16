4d17c503f72581ed4d482f0344e504a7ce5cc763
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-04-16 09:17:40 -0700

    fin 03 async await; init 04 async iteration

--- rhino/13_asynchronous/03_async_await.js
+++ rhino/13_asynchronous/03_async_await.js
@@ -1,1 +1,44 @@
 // 1. await
+let response = await fetch('/api/user/profile');
+let profile = await response.json();
+
+
+
+
+// 2. async functions
+/** Returns a Promise */
+async function getHighScore() {
+  let response = await fetch('/api/user/profile');
+  let profile = await response.json();
+  return profile.highScore;
+}
+
+displayHighscore(await getHighScore());
+getHighScore().then(displayHighScore).catch(console.error);
+
+
+
+// 3. Awaiting multiple promises
+async function getJSON(url) {
+  let response = await fetch(url);
+  let body = await response.json();
+  return body;
+}
+
+let val1 = await getJSON(url1);
+let val2 = await getJSON(url2);
+
+// better:
+let [val1, val2] = await Promise.all([getJSON(url1), getJSON(url2)]);
+
+
+
+// 4. Implementation details
+async function f(x) { /* body */; } // equiv to:
+
+function f(x) {
+  return new Promise(function(resolve, reject) {
+      try { resolve((function(x) { /* body */; })(x)); }
+      catch(e) { reject(e); }
+  });
+}
