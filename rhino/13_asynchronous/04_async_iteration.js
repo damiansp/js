f1edfa3cc1a349be1ea3036525f82ccf6f7cc5ee
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-04-22 15:15:13 -0700

    async iter++

--- rhino/13_asynchronous/04_async_iteration.js
+++ rhino/13_asynchronous/04_async_iteration.js
@@ -1,1 +1,25 @@
 // 1. The for/await loop
+const fs = require('fs');
+
+
+async function parseFile(filename) {
+  let stream = fs.createReadStream(filename, {encoding: 'utf-8'});
+  for await (let chunk of stream) parseChunk(chunk);
+}
+
+
+const urls = [url1, url2, url3];
+
+const promises = urls.map(url => fetch(url));
+
+for (const promise of promises) {
+  resp = await promise;
+  handle(resp);
+}
+
+// cleaner:
+for await (const resp of promises) handle(resp);
+
+
+
+// 3. Asynchronous generators
