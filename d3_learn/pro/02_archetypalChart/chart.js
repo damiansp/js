dc4dc44b221af6b9edfe803a741e1c3b5e43bf16
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-05-03 08:51:40 -0700

    init pro-d3

---
+++ d3_learn/pro/02_archetypalChart/chart.js
@@ -0,0 +1,46 @@
+let svg = d3.select('svg');
+const  margin = {top: 20, right: 20, bottom: 30, left: 40};
+const W = +svg.attr('width') - margin.left - margin.right;
+const H = +svg.attr('height') - margin.top - margin.bottom;
+
+let x = d3.scaleBand().rangeRound([0, W]).padding(0.1),
+  y = d3.scaleLinear().rangeRound([H, 0]);
+let g = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  
+d3.tsv('data.tsv')
+  .then((data) => {
+      return data.map((d) => {
+          d.frequency = +d.frequency;
+          return d;
+      });
+  }).then((data) => {
+      x.domain(data.map(function(d) { return d.letter; }));
+      y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+      g.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', `translate(0, ${H})`)
+        .call(d3.axisBottom(x));
+      g.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3.axisLeft(y).ticks(10, '%'))
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('text-anchor', 'end')
+        .text('Frequency');
+      g.selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d) { return x(d.letter); })
+        .attr('y', function(d) { return y(d.frequency); })
+        .attr('width', x.bandwidth())
+        .attr('height', function(d) { return H - y(d.frequency); });
+  })
+  .catch((err) => { throw err; });
+
+
---
+++ d3_learn/pro/README.md
@@ -0,0 +1,3 @@
+# Coding along with
+Iglesias, M. (2019). _Pro-d3.js_ (Apress)
+<a href="https://github.com/Apress/pro-d3.js">Repo</a>
