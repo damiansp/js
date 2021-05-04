0e9778ed2a54b7dac4173879f462ba41ee3b7343
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-05-04 06:51:50 -0700

    async iter++

--- rhino/13_asynchronous/04_async_iteration.js
+++ rhino/13_asynchronous/04_async_iteration.js
@@ -23,3 +23,49 @@ for await (const resp of promises) handle(resp);
 
 
 // 3. Asynchronous generators
+function elapsedTime(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function* clock(interval, max=Infinity) {
+  for (let count = 1; count <= max; count++) {
+    await elapsedTime(interval);
+    yield count;
+  }
+}
+
+
+async function test() {
+  for await (let tick of clock(300, 100)) { // loop 100x every 300ms
+      console.log(tick);
+  }
+}
+
+
+
+// 4. Implementing Asynchronous Iterators
+function clock(interval, max=Infinity) {
+
+  function until(time) {
+    return new Promise(resolve => setTimeout(resolve, time - Date.now()));
+  }
+
+  return {
+    startTime: Date.now(),
+    count: 1,
+    async next() {
+      if (this.count > max) return {done: true};
+      let targetTime = this.startTime + this.count*interval;
+      await until(targetTime);
+      return {value: this.count++};
+    },
+    [Symbol.asyncIterator]() { return this; }
+  };
+}
+
+
+
+
+
+    
